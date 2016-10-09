b96489806bdfa86d50bdc73848855814e6c2f289
gautier-gdx <gautier.gedoux@isep.fr>
2016-10-09 18:25:58 +0200

    adding carthage to example

---
+++ Example/Carthage/Checkouts/SVGView/Example/Example/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Gautier Gdx on 09/10/2016.
+//  Copyright © 2016 Swapcard. All rights reserved.
+//
+
+import UIKit
+//import SVGView
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let svgView =
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Example/Carthage/Checkouts/SVGView/SVGView/SVGView.h
@@ -0,0 +1,19 @@
+//
+//  SVGView.h
+//  SVGView
+//
+//  Created by Gautier Gdx on 09/10/2016.
+//  Copyright © 2016 Swapcard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVGView.
+FOUNDATION_EXPORT double SVGViewVersionNumber;
+
+//! Project version string for SVGView.
+FOUNDATION_EXPORT const unsigned char SVGViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGView/PublicHeader.h>
+
+
